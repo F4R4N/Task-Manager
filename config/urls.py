75c44a5c8782de393d04bdc4a906c8551fffa611
Faran <farantgh@gmail.com>
2026-03-11 00:24:25 +0330

    add simplejwt login, refresh and logout endpoints

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenBlacklistView, TokenRefreshView
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('task/', include('tasks.urls')),
+    path("auth/login/", TokenObtainPairView.as_view(), name="login"),
+    path("auth/logout/", TokenBlacklistView.as_view(), name="logout"),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="refresh"),
 ]
