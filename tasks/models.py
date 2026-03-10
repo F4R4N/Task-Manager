79d627f3ea72d002f5db5269c3a2da0c0b2e2ec3
Faran <farantgh@gmail.com>
2026-03-11 00:25:23 +0330

    add assignee and priority to model

--- tasks/models.py
+++ tasks/models.py
@@ -8,13 +8,30 @@ class Task(models.Model):
         TODO = "todo", "Todo"
         IN_PROGRESS = "in_progress", "In Progress"
         DONE = "done", "Done"
+
+    class Priority(models.TextChoices):
+        LOW = "low", "Low"
+        MEDIUM = "medium", "Medium"
+        HIGH = "high", "High"
+
     title = models.CharField(
         max_length=50
     )
     description = models.TextField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="task_owner_set"
+    )
     status = models.CharField(
         max_length=20, choices=Status.choices, default=Status.TODO
+    )
+    assignee = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True,
+        related_name="task_assignee_set"
+    )
+    priority = models.TextField(
+        max_length=20,
+        choices=Priority.choices,
+        default=Priority.MEDIUM
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
