c15a0cd7635f19c546afdc25d3d1ccacc1f9c61d
Faran <farantgh@gmail.com>
2026-03-12 01:03:58 +0330

    add filter for task and ordering

--- task/views.py
+++ task/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import filters
 from .serializers import TaskSerializer
 from .permissions import IsOwnerOrReadOnly
 from .models import Task
@@ -9,6 +10,9 @@ class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    ordering = ["-created_at"]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title", "description"]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
