1bd71358252baee24676f084d3e18c1738ac8085
Faran <farantgh@gmail.com>
2026-03-13 23:23:38 +0330

    change gravatar size to fit profile picture size

--- Task-Management/customauth/utils.py
+++ Task-Management/customauth/utils.py
@@ -5,4 +5,4 @@ import hashlib
 def get_user_gravatar_link(email: str) -> str:
     email = email.lower().encode("utf-8")
     hashed = hashlib.md5(email).hexdigest()
-    return settings.GRAVATAR_URL + hashed + "?s=200&d=identicon"
+    return settings.GRAVATAR_URL + hashed + "?s=32&d=identicon"
