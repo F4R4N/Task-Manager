8f5fb5540656e639903635fc1ced1a5121794505
Faran <farantgh@gmail.com>
2026-03-13 23:23:10 +0330

    add endpoint to get the logged in account

--- Task-Management/customauth/urls.py
+++ Task-Management/customauth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginView, LogoutView, RefreshView
+from .views import LoginView, LogoutView, RefreshView, AccountView
 
 app_name = "customauth"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("refresh/", RefreshView.as_view(), name="refresh"),
+    path("account/", AccountView.as_view(), name="account"),
 ]
--- Task-Management/customauth/views.py
+++ Task-Management/customauth/views.py
@@ -1,9 +1,10 @@
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenBlacklistView, TokenRefreshView
 )
-from .serializers import LoginSerializer
-from rest_framework.permissions import AllowAny
+from .serializers import LoginSerializer, UserSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 class LoginView(TokenObtainPairView):
@@ -50,3 +51,11 @@ class RefreshView(TokenRefreshView):
         request.data["refresh"] = refresh
 
         return super().post(request, *args, **kwargs)
+
+
+class AccountView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
