c6f382f79a954982324e36fdd4ffe1f5f56983e8
Faran <farantgh@gmail.com>
2026-03-15 00:41:49 +0330

    show tasks on the board

---
+++ Task-Management-UI/components/taskCard.js
@@ -0,0 +1,35 @@
+import { fetchTasks } from "../js/api.js";
+import { formatDate } from "./helper.js"
+
+export async function addTaskCard(task) {
+    const template = document.getElementById("task-template");
+    if (!template) return;
+
+    const clone = template.content.cloneNode(true);
+
+    const title = clone.querySelector(".card-title")
+    title.textContent = task.title;
+    title.title = task.title;
+    clone.querySelector(".created-at").textContent = formatDate(task.created_at);
+
+    const badge = clone.querySelector(".badge")
+    badge.textContent = task.priority;
+    badge.classList.add("badge-" + task.priority);
+    if (task.assignee != null) {
+        clone.querySelector(".assignee-username").textContent = task.assignee.username;
+        const assigneeProfilePic = clone.querySelector(".assignee-profile-pic")
+        assigneeProfilePic.src = task.assignee.gravatar;
+        assigneeProfilePic.alt = task.assignee.username + " profile picture"
+    }else{
+        clone.querySelector(".assignee").remove()
+    }
+
+    const container = document.getElementById(task.status);
+    container.querySelector(".cards").appendChild(clone);
+}
+
+export async function renderTasks() {
+    const tasks = await fetchTasks();
+    tasks.forEach(task => addTaskCard(task));
+}
+
--- Task-Management-UI/js/app.js
+++ Task-Management-UI/js/app.js
@@ -1,12 +1,12 @@
 import { loadNavbar, renderUserNavbar } from '../components/navbar/navbar.js';
 import { loadSideMenu } from "../components/sideMenu/sideMenu.js";
-
+import { renderTasks } from "../components/taskCard.js"
 async function init() {
     const appContainer = document.getElementById("app");
     await loadSideMenu(appContainer);
     await loadNavbar();
     await renderUserNavbar();
-
+    await renderTasks();
 }
 
 init();
