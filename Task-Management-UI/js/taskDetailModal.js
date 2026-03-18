21928ce492cedd5575d42eff58b284f3fb54d1d0
Faran <farantgh@gmail.com>
2026-03-18 07:54:43 +0330

    add task detail modal

---
+++ Task-Management-UI/js/taskDetailModal.js
@@ -0,0 +1,72 @@
+import { fetchTask } from "./api.js";
+import { formatDate } from "./helper.js";
+
+function closeTaskDetailModal(modal) {
+    modal.classList.remove('is-visible');
+    document.body.style.overflow = '';
+}
+
+function openTaskModal(modal, taskData) {
+    const modalTaskContent = document.getElementById("modalTaskContent");
+    modalTaskContent.innerHTML = '';
+    if (taskData) {
+        modalTaskContent.innerHTML = `
+                <h2 class="task-title">${taskData.title}</h2>
+                <div class="task-detail-item">
+                    <strong>Description:</strong>
+                    <p>${taskData.description || 'N/A'}</p>
+                </div>
+                <div class="task-detail-item">
+                    <strong>Assignee:</strong>
+                    ${taskData.assignee
+                        ? `<span>
+                            <img src="${taskData.assignee.gravatar}" alt="${taskData.assignee.username} Profile Picture" class="assignee-avatar"> 
+                            ${taskData.assignee.username}
+                           </span>`
+                        : '<span>N/A</span>'
+                    }
+                </div>
+                <div class="task-detail-item">
+                    <strong>Priority:</strong>
+                    <span class="badge badge-${taskData.priority}">${taskData.priority}</span>
+                </div>
+                <div class="task-detail-item">
+                    <strong>Status:</strong>
+                    <span>${taskData.status[0].toUpperCase() + taskData.status.slice(1)}</span>
+                </div>
+                <div class="task-detail-item">
+                    <strong>Created:</strong>
+                    <span>${formatDate(taskData.created_at)} (Last Updated at: ${formatDate(taskData.updated_at)})</span>
+                </div>
+            `;
+    } else {
+        modalTaskContent.innerHTML = '<p>Could not load task details.</p>';
+    }
+
+    modal.classList.add('is-visible');
+    document.body.style.overflow = 'hidden';
+}
+
+export async function showDetailModal(taskId) {
+    const modal = document.getElementById("taskModal");
+    const closeButton = modal.querySelector(".close-btn");
+
+    const task = await fetchTask(taskId);
+    openTaskModal(modal, task);
+
+    closeButton.addEventListener("click", () => {
+        closeTaskDetailModal(modal);
+    });
+
+    modal.addEventListener("click", (event) => {
+        if (event.target === modal) {
+            closeTaskDetailModal(modal);
+        }
+    });
+
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Escape" && modal.classList.contains('is-visible')) {
+            closeTaskDetailModal(modal);
+        }
+    });
+}
