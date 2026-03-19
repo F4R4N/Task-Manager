0c0648b48e9763ebe1d72aaa42681cbe1cc5abf4
Faran <farantgh@gmail.com>
2026-03-20 02:18:49 +0330

    moved

--- Task-Management-UI/js/Tasks.js
+++ Task-Management-UI/js/Tasks.js
@@ -4,20 +4,22 @@ import { renderModal } from "../js/mainModal.js";
 import { showDetailModal } from "../js/sideModal.js";
 
 
-export function fillTaskData(task) {
+export function createTaskCard(task) {
     const template = document.getElementById("task-template");
     if (!template) return;
-
+    // create clone of the template
     const clone = template.content.cloneNode(true);
-    const card = clone.querySelector(".card");
-    const title = clone.querySelector(".card-title")
+    // fill title data
+    const title = clone.querySelector(".card-title");
     title.textContent = task.title;
     title.title = task.title;
+    // fill created at data
     clone.querySelector(".created-at").textContent = formatDate(task.created_at);
-
+    // fill badge data
     const badge = clone.querySelector(".badge")
     badge.textContent = capitalize(task.priority);
     badge.classList.add("badge-" + task.priority);
+    // fill assignee data
     if (task.assignee != null) {
         clone.querySelector(".assignee-username").textContent = task.assignee.username;
         const assigneeProfilePic = clone.querySelector(".assignee-profile-pic")
@@ -26,27 +28,29 @@ export function fillTaskData(task) {
     } else {
         clone.querySelector(".assignee").remove()
     }
-    card.dataset.id = task.id;
 
+    const card = clone.querySelector(".card");
+    card.dataset.id = task.id;
     card.addEventListener("click", (e) => {
         showDetailModal(e.currentTarget.dataset.id);
-    })
+    });
+
     return clone;
 }
 
-export async function addTaskCard(task) {
-    const card = fillTaskData(task);
+function addCardToContainers(task) {
+    const card = createTaskCard(task);
 
     const container = document.getElementById(task.status);
     container.querySelector(".cards").appendChild(card);
 }
 
-export async function renderTasks() {
+export async function renderAllTasks() {
     const tasks = await fetchTasks();
-    tasks.forEach(task => addTaskCard(task));
+    tasks.forEach(task => addCardToContainers(task));
 }
 
-export async function displayAddTask(res) {    
+export async function showAddTaskButtons(res) {    
     const addTaskBtns = document.querySelectorAll(".add");
     if (!res.ok) {
         addTaskBtns.forEach(btn => {
