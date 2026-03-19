ad666c71b1f28e81f2d53cf7491c4c8f3e0ba51b
Faran <farantgh@gmail.com>
2026-03-19 23:14:43 +0330

    add delete functionality to renderModal

--- Task-Management-UI/js/app.js
+++ Task-Management-UI/js/app.js
@@ -1,8 +1,10 @@
 import { loadNavbar } from '../components/navbar/navbar.js';
 import { loadSideMenu } from "../components/sideMenu/sideMenu.js";
 import { renderTasks, displayAddTask } from "../components/Tasks.js"
-import { fetchWithAuth } from './api.js';
+import { fetchWithAuth, deleteTask } from './api.js';
 import { renderSearchModal } from './searchModal.js';
+import { closeTaskDetailModal } from './taskDetailModal.js';
+import { renderModal } from './taskModal.js';
 
 async function init() {
     const res = await fetchWithAuth("/auth/account");
@@ -12,6 +14,23 @@ async function init() {
     await displayAddTask(res);
     await renderTasks();
     await renderSearchModal();
+    attachEventListeners();
+}
+
+function attachEventListeners() {
+    const modal = document.getElementById("taskModal");
+
+    document.addEventListener("click", (e) => {
+        if (e.target.matches("#deleteBtn")) {
+            console.log(modal)
+            closeTaskDetailModal(modal);
+            renderModal("delete", e.target.dataset.id)
+        }
+        if (e.target.matches('.confirm-delete')) {
+            deleteTask(e.target.dataset.id);
+
+        }
+    });
 }
 
 init();
--- Task-Management-UI/js/taskModal.js
+++ Task-Management-UI/js/taskModal.js
@@ -1,4 +1,4 @@
-import { fetchWithAuth } from "./api.js";
+import { fetchWithAuth, deleteTask } from "./api.js";
 
 export async function readModalFields(modal) {
     const actionBtn = modal.querySelector("#modalActionBtn");
@@ -20,36 +20,42 @@ export async function readModalFields(modal) {
 
 }
 
-export async function renderModal(action, task, status) {
+export async function renderModal(action, taskId, status) {
     const modal = document.getElementById("modalOverlay");
-    
-    const usersRes = await fetchWithAuth("/users");
-    const users = await usersRes.json();
-    const assigneesSelect = modal.querySelector("#assigneeSelect")
-    assigneesSelect.innerHTML = "";
-    users.unshift({ id: null, username: null, email: null })
-    users.forEach(user => {
-        const option = document.createElement("option");
-        option.value = user.id || "";
-        option.textContent = `${user.username || "--"} ${user.username ? `(${user.email})` : ""}`;
-
-        assigneesSelect.appendChild(option)
-    });
     const actionBtn = modal.querySelector("#modalActionBtn");
-    actionBtn.dataset.action = action;
 
-    if (action === "create") {
-        modal.querySelector("#modalHeader").textContent = "Create Task"
-        modal.querySelector('#statusSelect').value = status;
+    if (action === "delete") {
+        modal.querySelector("#modalHeader").textContent = "Are you sure you want to delete this task?";
+        const taskModal = modal.querySelector('.task-modal');
+        taskModal.style.display = "none";
         actionBtn.textContent = action[0].toUpperCase() + action.slice(1);
-
+        const confirmDeleteBtn = modal.querySelector("button");
+        confirmDeleteBtn.dataset.id = taskId;
+        confirmDeleteBtn.style.display = "block";
     } else {
+        const usersRes = await fetchWithAuth("/users");
+        const users = await usersRes.json();
+        const assigneesSelect = modal.querySelector("#assigneeSelect")
+        assigneesSelect.innerHTML = "";
+        users.unshift({ id: null, username: null, email: null })
+        users.forEach(user => {
+            const option = document.createElement("option");
+            option.value = user.id || "";
+            option.textContent = `${user.username || "--"} ${user.username ? `(${user.email})` : ""}`;
+            assigneesSelect.appendChild(option)
+        });
+        actionBtn.dataset.action = action;
+        if (action === "create") {
+            modal.querySelector("#modalHeader").textContent = "Create Task"
+            modal.querySelector('#statusSelect').value = status;
+            actionBtn.textContent = action[0].toUpperCase() + action.slice(1);
+            modal.querySelector(".task-modal").style.display = "block";
 
+        }
     }
 
     modal.style.display = 'flex';
-    modal.querySelector(".task-modal").style.display = "block";
-    modal.querySelector(".search-modal").style.display = "none";
+    modal.querySelector(".search-modal").style.display = "none !important";
     document.getElementById("modalActionBtn").style.display = "block";
 
 }
