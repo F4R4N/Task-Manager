f3453010350a35b840cd5d3dd4a2205ddd234465
Faran <farantgh@gmail.com>
2026-03-23 22:27:31 +0330

    remove browsable API

--- Task-Manager/config/settings.py
+++ Task-Manager/config/settings.py
@@ -131,14 +131,13 @@ STATIC_URL = "static/"
 
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    "DEFAULT_RENDERER_CLASSES": ['rest_framework.renderers.JSONRenderer'],
 }
 
 
