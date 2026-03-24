f83557f5cf7a1fcbbce74cd05fa151149adb738f
Faran <farantgh@gmail.com>
2026-03-24 22:37:00 +0330

    add permission to check project membership

--- Task-Manager/task/permissions.py
+++ Task-Manager/task/permissions.py
@@ -1,9 +1,31 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from .models import ProjectMember
 
 
 class IsOwnerOrReadOnly(BasePermission):
+    """
+    Only allow owner of the task to use unsafe methods.
+    """
+
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
 
         return obj.owner == request.user
+
+
+class IsProjectMember(BasePermission):
+    """
+    Only allow members of the project to access the view.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+
+        if not user.is_authenticated:
+            return False
+
+        return ProjectMember.objects.filter(
+            project=obj,
+            user=user,
+        ).exists()
