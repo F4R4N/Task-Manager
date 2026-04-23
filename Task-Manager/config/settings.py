dd10ce1ad80e6b1dfccb2e944e1875c97df146a2
Faran <farantgh@gmail.com>
2026-04-23 23:05:57 +0330

    add browseable api interface

--- Task-Manager/config/settings.py
+++ Task-Manager/config/settings.py
@@ -137,7 +137,10 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-    "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
 }
 
 
